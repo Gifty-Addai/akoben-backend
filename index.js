ed2d54df3a3291370e08e07b0d92992a206a32ea
Frank Addai <faddai@acsint.io>
2026-05-19 15:12:53 +0000

    refactor: remove hardcoded localhost origins in favor of environment variable configurations

--- index.js
+++ index.js
@@ -35,10 +35,6 @@ const parseOrigins = (originsString) => {
 };
 
 const allowedOrigins = [
-  'http://localhost:5173',
-  'http://localhost:5175',
-  'http://localhost:5174',
-  'http://localhost:3000',
   ...parseOrigins(process.env.FRONTEND_URL),
   ...parseOrigins(process.env.PRODUCTION_FRONTEND_URL),
   ...parseOrigins(process.env.ALLOWED_ORIGINS)
